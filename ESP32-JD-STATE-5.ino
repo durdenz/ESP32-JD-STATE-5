b0dc1f890a1461784ac58a417c0ce6bb2460d649
George Durden <george@durden.com>
2025-03-04 16:40:21 -0600

    Fix Rotation and Add PlayGIF iterations

--- ESP32-JD-STATE-5.ino
+++ ESP32-JD-STATE-5.ino
@@ -7,6 +7,7 @@
 #include <FS.h>
 #include <SD.h>
 #include <AnimatedGIF.h>
+#include <JPEGDecoder.h>
 
 TFT_eSPI tft = TFT_eSPI();
 AnimatedGIF gif;
@@ -31,8 +32,8 @@ bool AnimateGIF = false;
 SPIClass touchscreenSPI = SPIClass(VSPI);
 XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
 #define FONT_SIZE 2
 
 // Define Button 1
@@ -59,7 +60,7 @@ int newState = 0;
 bool stateChange = false;
 
 // Standby Timer Variables
-#define STANDBY_DURATION 1
+#define STANDBY_DURATION 1  // Inactivity Timeout in Minutes
 time_t standbyStart = now();
 
 // Function to see if Button 1 has been pressed
@@ -121,7 +122,7 @@ void IRAM_ATTR onTimer0()
   // Lock mutex during ISR
   portENTER_CRITICAL_ISR(&timerMux);
 
-  Serial.println("Timer0 IRQ Handler Entered");
+  // Serial.println("Timer0 IRQ Handler Entered");
 
   // Checks if Touchscreen was touched, then checks if button pressed and changes state
   if (touchscreen.tirqTouched() && touchscreen.touched()) {
@@ -133,6 +134,8 @@ void IRAM_ATTR onTimer0()
     y = map(p.y, 240, 3800, 1, SCREEN_HEIGHT);
     z = p.z;
 
+    // Touch event resets inactivity timer 
+    standbyStart = now();
 
     if (currentState == STATE_ACTIVE) {
       // Check and see if touch was pressing Button 1
@@ -153,7 +156,7 @@ void IRAM_ATTR onTimer0()
     }
   }
 
-  // Unlock mutex ISR exit
+  // Unlock mutex on ISR exit
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
@@ -199,16 +202,16 @@ void StateVending() {
   int textY = 80;
  
   // Print to screen
-  String tempText = "Vending State";
+  // String tempText = "Vending State";
 
   // Call Function to dispense Soda
   //
   dispenseSoda();
 
-  tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
+  // tft.drawCentreString(tempText, centerX, textY, FONT_SIZE);
 
   gifIndex = 0; // gif for Dispense State
-  PlayGIF(gifFiles[gifIndex], 5); // Play GIF off of SD card for 5 seconds
+  PlayGIF(gifFiles[gifIndex], 1, 5); // Play GIF off of SD card for 5 seconds
   // delay(1000);
   
   // Vending is complete - Set State to Active 
@@ -246,7 +249,7 @@ void StateActive() {
 
   // PlayGIF
   gifIndex = 2; // gif for Active State
-  PlayGIF(gifFiles[gifIndex], 5); // Play GIF off of SD card for 5 seconds
+  PlayGIF(gifFiles[gifIndex], 5, 20); // Play GIF off of SD card for 5 seconds
 
   // Start Time for Inactive Timer
   standbyStart = now();
@@ -278,7 +281,9 @@ void StateStandBy() {
 
   // PlayGIF
   gifIndex = 1; // gif for StandBy State
-  PlayGIF(gifFiles[gifIndex], 5); // Play GIF off of SD card for 5 seconds
+  PlayGIF(gifFiles[gifIndex], 1, 5); // Play GIF off of SD card for 1 iteration or no longer than 5 seconds
+
+  // drawSdJpeg("/lena20k.jpg", 0, 0);     // This draws a jpeg pulled off the SD Card
 }
 
 void setup() {
@@ -289,18 +294,18 @@ void setup() {
   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
   touchscreen.begin(touchscreenSPI);
 
-  // Set the Touchscreen rotation in landscape mode
+  // Set the Touchscreen rotation in portrait mode
   // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 3: touchscreen.setRotation(3);
   //
-  touchscreen.setRotation(3);
+  touchscreen.setRotation(4);
 
   // Start the tft display
   //
   tft.init();
 
-  // Set the TFT display rotation in landscape mode
+  // Set the TFT display rotation in portait mode
   //
-  tft.setRotation(1);
+  tft.setRotation(2);
 
   // Clear the screen before writing to it
   //
@@ -341,10 +346,11 @@ void loop() {
 // ======================================================
 // BEGIN - GIF Support Functions
 //
-bool PlayGIF(String gifFile, int seconds) {
+bool PlayGIF(String gifFile, int iterations, int seconds) {
   time_t startTime = now();
+  int iCount = 1;
 
-  Serial.println("PlayGIF: Entered");
+  Serial.printf("PlayGIF: Entered - Iteration = %d\n", iCount);
 
   AnimateGIF = true;
   while (AnimateGIF == true) {
@@ -367,9 +373,14 @@ bool PlayGIF(String gifFile, int seconds) {
       Serial.println("PlayGIF: Time Exceeded");
       AnimateGIF = false;
     }
+    if (iCount++ > iterations) {
+      Serial.printf("PlayGIF: Iterations Exceeded, iCount = %d\n", iCount);
+      AnimateGIF = false;
+    }
     if (AnimateGIF == false) {
       return(true);
     }
+    Serial.printf("PlayGIF: Iteration iCount = %d\n", iCount);
   }
 }
 
@@ -596,5 +607,180 @@ void GIFDraw(GIFDRAW *pDraw)
   }
   tft.endWrite();
 } /* GIFDraw() */
+
+// ======================================================
+// BEGIN - JPEG Support Functions
+//
+
+//####################################################################################################
+// Draw a JPEG on the TFT pulled from SD Card
+//####################################################################################################
+// xpos, ypos is top left corner of plotted image
+void drawSdJpeg(const char *filename, int xpos, int ypos) {
+
+  // Open the named file (the Jpeg decoder library will close it)
+  File jpegFile = SD.open( filename, FILE_READ);  // or, file handle reference for SD library
+ 
+  if ( !jpegFile ) {
+    Serial.print("ERROR: File \""); Serial.print(filename); Serial.println ("\" not found!");
+    return;
+  }
+
+  Serial.println("===========================");
+  Serial.print("Drawing file: "); Serial.println(filename);
+  Serial.println("===========================");
+
+  // Use one of the following methods to initialise the decoder:
+  bool decoded = JpegDec.decodeSdFile(jpegFile);  // Pass the SD file handle to the decoder,
+  //bool decoded = JpegDec.decodeSdFile(filename);  // or pass the filename (String or character array)
+
+  if (decoded) {
+    // print information about the image to the serial port
+    jpegInfo();
+    // render the image onto the screen at given coordinates
+    jpegRender(xpos, ypos);
+  }
+  else {
+    Serial.println("Jpeg file format not supported!");
+  }
+}
+
+//####################################################################################################
+// Draw a JPEG on the TFT, images will be cropped on the right/bottom sides if they do not fit
+//####################################################################################################
+// This function assumes xpos,ypos is a valid screen coordinate. For convenience images that do not
+// fit totally on the screen are cropped to the nearest MCU size and may leave right/bottom borders.
+void jpegRender(int xpos, int ypos) {
+
+  //jpegInfo(); // Print information from the JPEG file (could comment this line out)
+
+  uint16_t *pImg;
+  uint16_t mcu_w = JpegDec.MCUWidth;
+  uint16_t mcu_h = JpegDec.MCUHeight;
+  uint32_t max_x = JpegDec.width;
+  uint32_t max_y = JpegDec.height;
+
+  bool swapBytes = tft.getSwapBytes();
+  tft.setSwapBytes(true);
+  
+  // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+  // Typically these MCUs are 16x16 pixel blocks
+  // Determine the width and height of the right and bottom edge image blocks
+  uint32_t min_w = jpg_min(mcu_w, max_x % mcu_w);
+  uint32_t min_h = jpg_min(mcu_h, max_y % mcu_h);
+
+  // save the current image block size
+  uint32_t win_w = mcu_w;
+  uint32_t win_h = mcu_h;
+
+  // record the current time so we can measure how long it takes to draw an image
+  uint32_t drawTime = millis();
+
+  // save the coordinate of the right and bottom edges to assist image cropping
+  // to the screen size
+  max_x += xpos;
+  max_y += ypos;
+
+  // Fetch data from the file, decode and display
+  while (JpegDec.read()) {    // While there is more data in the file
+    pImg = JpegDec.pImage ;   // Decode a MCU (Minimum Coding Unit, typically a 8x8 or 16x16 pixel block)
+
+    // Calculate coordinates of top left corner of current MCU
+    int mcu_x = JpegDec.MCUx * mcu_w + xpos;
+    int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+
+    // check if the image block size needs to be changed for the right edge
+    if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
+    else win_w = min_w;
+
+    // check if the image block size needs to be changed for the bottom edge
+    if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
+    else win_h = min_h;
+
+    // copy pixels into a contiguous block
+    if (win_w != mcu_w)
+    {
+      uint16_t *cImg;
+      int p = 0;
+      cImg = pImg + win_w;
+      for (int h = 1; h < win_h; h++)
+      {
+        p += mcu_w;
+        for (int w = 0; w < win_w; w++)
+        {
+          *cImg = *(pImg + w + p);
+          cImg++;
+        }
+      }
+    }
+
+    // calculate how many pixels must be drawn
+    uint32_t mcu_pixels = win_w * win_h;
+
+    // draw image MCU block only if it will fit on the screen
+    if (( mcu_x + win_w ) <= tft.width() && ( mcu_y + win_h ) <= tft.height())
+      tft.pushImage(mcu_x, mcu_y, win_w, win_h, pImg);
+    else if ( (mcu_y + win_h) >= tft.height())
+      JpegDec.abort(); // Image has run off bottom of screen so abort decoding
+  }
+
+  tft.setSwapBytes(swapBytes);
+
+  showTime(millis() - drawTime); // These lines are for sketch testing only
+}
+
+//####################################################################################################
+// Print image information to the serial port (optional)
+//####################################################################################################
+// JpegDec.decodeFile(...) or JpegDec.decodeArray(...) must be called before this info is available!
+void jpegInfo() {
+
+  // Print information extracted from the JPEG file
+  Serial.println("JPEG image info");
+  Serial.println("===============");
+  Serial.print("Width      :");
+  Serial.println(JpegDec.width);
+  Serial.print("Height     :");
+  Serial.println(JpegDec.height);
+  Serial.print("Components :");
+  Serial.println(JpegDec.comps);
+  Serial.print("MCU / row  :");
+  Serial.println(JpegDec.MCUSPerRow);
+  Serial.print("MCU / col  :");
+  Serial.println(JpegDec.MCUSPerCol);
+  Serial.print("Scan type  :");
+  Serial.println(JpegDec.scanType);
+  Serial.print("MCU width  :");
+  Serial.println(JpegDec.MCUWidth);
+  Serial.print("MCU height :");
+  Serial.println(JpegDec.MCUHeight);
+  Serial.println("===============");
+  Serial.println("");
+}
+
+//####################################################################################################
+// Show the execution time (optional)
+//####################################################################################################
+// WARNING: for UNO/AVR legacy reasons printing text to the screen with the Mega might not work for
+// sketch sizes greater than ~70KBytes because 16-bit address pointers are used in some libraries.
+
+// The Due will work fine with the HX8357_Due library.
+
+void showTime(uint32_t msTime) {
+  //tft.setCursor(0, 0);
+  //tft.setTextFont(1);
+  //tft.setTextSize(2);
+  //tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  //tft.print(F(" JPEG drawn in "));
+  //tft.print(msTime);
+  //tft.println(F(" ms "));
+  Serial.print(F(" JPEG drawn in "));
+  Serial.print(msTime);
+  Serial.println(F(" ms "));
+}
+
+// END -- JPEG Support Functions
+// ======================================================
+
 
 
